c19f7ef96a0c9884dc4ff893304ccf71204d1f39
Daniel Beilin <beilin@savvy.security>
2024-12-26 15:13:35 +0200

    add CORS

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,24 @@ export default defineConfig({
   server: {
     port: 5173,
     open: false,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'https://maplestory.io/api/GMS/83',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'https://maplestory.io/api/GMS/83',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
